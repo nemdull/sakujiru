ae9cc53f62b4df28e7b39cd12fe573fc98ccd6ca
dlanx0607 <dlanx@outlook.jp>
2022-03-03 14:19:22 +0900

    tag-1

---
+++ db/migrate/20220303043604_add_categoryid_to_post.rb
@@ -0,0 +1,5 @@
+class AddCategoryidToPost < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_20_070020) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_03_044443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,12 +22,28 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_20_070020) do
     t.index ["post_id"], name: "index_arts_on_post_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "art_name"
     t.string "art_text"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -46,5 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_20_070020) do
   end
 
   add_foreign_key "arts", "posts"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
