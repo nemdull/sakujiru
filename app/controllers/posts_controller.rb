07bb5db3b8c8fb4e99d2056cd5a86b2c83acc104
nemdull <dlanx@outlook.jp>
2022-03-04 18:31:27 +0900

    fix post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_post, only: %i(show destroy)
-  before_action :set_parents
 
   def new
     @post = Post.new
