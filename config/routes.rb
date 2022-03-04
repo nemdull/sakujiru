840b64576da4360d6d572df516a309b2bc423e6a
dlanx0607 <dlanx@outlook.jp>
2022-03-04 15:05:50 +0900

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/users/:id",to: "users#show", as: "user"
 
