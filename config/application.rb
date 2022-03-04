b3db795585261c7c47ede227ff94009bce05de71
nemdull <dlanx@outlook.jp>
2022-03-04 17:32:51 +0900

    delete sassc-rails&admin

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Sakujiru
   class Application < Rails::Application
+    config.assets.css_compressor = nil
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,6 +17,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('daisyui'),
+    require('daisyui')
   ]
 }
